0e97c8b2151a3c637a2b980b92ae482e4206df56
Guest User <Guest@epicodus-4.local>
2017-10-02 14:46:57 -0700

    Add pokemon model.

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from '../player.model';
+import { Pokemon } from '../pokemon.model';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-battle',
   templateUrl: './battle.component.html',
-  styleUrls: ['./battle.component.css']
+  styleUrls: ['./battle.component.css'],
+  providers: [PokemonService, Pokemon]
 })
 export class BattleComponent implements OnInit {
 
-  constructor() { }
+  public player: Player;
+  public myPokemon: Pokemon;
+  public opponent: Pokemon;
 
-  ngOnInit() {
+  constructor(public pokemonService: PokemonService) {
+
   }
 
+  ngOnInit() {
+    this.pokemonService.getData("https://pokeapi.co/api/v2/pokemon/5/").subscribe(returnedJSON => {
+      console.log(returnedJSON);
+    },
+      returnedJSON => {
+        console.log("ERROR: ", returnedJSON);
+      },
+      () => {
+        console.log("Completed");
+      }
+    );
+  }
 }
