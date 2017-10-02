a2a96e494bad64ffb82bcc7afbc61794c516e980
Guest User <Guest@epicodus-4.local>
2017-10-02 16:49:17 -0700

    More work implemented on Battle Component.

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -7,28 +7,41 @@ import { PokemonService } from '../pokemon.service';
   selector: 'app-battle',
   templateUrl: './battle.component.html',
   styleUrls: ['./battle.component.css'],
-  providers: [PokemonService, Pokemon]
+  providers: [PokemonService]
 })
 export class BattleComponent implements OnInit {
 
   public player: Player;
   public myPokemon: Pokemon;
   public opponent: Pokemon;
+  public battling = true;
 
   constructor(public pokemonService: PokemonService) {
 
+    this.myPokemon = new Pokemon('charmander', ['fire', 'dragon'], 3, 100, 400, 600, 49, 50, ['tail lash', 'char'], ['all'], true);
+
+    this.opponent = new Pokemon('bulbasaur', ['grass', 'dragon'], 3, 100, 400, 600, 49, 50, ['vine whip', 'sunbeam'], ['all'], false);
+
+    this.commenceBattle();
   }
 
   ngOnInit() {
-    this.pokemonService.getData("https://pokeapi.co/api/v2/pokemon/5/").subscribe(returnedJSON => {
-      console.log(returnedJSON);
-    },
-      returnedJSON => {
-        console.log("ERROR: ", returnedJSON);
-      },
-      () => {
-        console.log("Completed");
-      }
-    );
+    // console.log('test');
+    // this.pokemonService.getData("https://pokeapi.co/api/v2/pokemon/5/").subscribe(returnedJSON => {
+    //   console.log(returnedJSON);
+    // })
+  }
+  commenceBattle() {
+    let turn = 1;
+    do {
+      this.myPokemon.currentHP--);
+      this.checkBattleOver();
+    } while (this.battling);
+  }
+
+  checkBattleOver() {
+    if(this.myPokemon.currentHP <= 0 || this.opponent.currentHP <= 0) {
+      this.battling = false;
+    }
   }
 }
--- src/app/pokemon.model.ts
+++ src/app/pokemon.model.ts
@@ -2,15 +2,15 @@ export class Pokemon {
 
   constructor(
     public name: string,
-    public type: string[],
+    public types: string[],
     public level: number,
     public currentLevelXP: number,
     public totalLevelXP: number,
     public totalAccruedXP: number,
-    public currentHP: number;
-    public maxHP: number;
-    public activeMoves: string[];
-    public allMoves: string[];
-    public playerActive: boolean;
+    public currentHP: number,
+    public maxHP: number,
+    public activeMoves: string[],
+    public allMoves: string[],
+    public playerActive: boolean
   ) {}
 }
