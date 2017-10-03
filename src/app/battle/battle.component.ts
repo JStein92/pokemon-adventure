4cc57d84624ae4e20dbb9361595133f8cd9840b7
Guest User <Guest@epicodus-4.local>
2017-10-03 10:02:12 -0700

    Add battle state conditions.

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -13,14 +13,18 @@ export class BattleComponent implements OnInit {
 
   public player: Player;
   public myPokemon: Pokemon;
+  public myPokemon2: Pokemon;
   public opponent: Pokemon;
   public battling = true;
 
   constructor(public pokemonService: PokemonService) {
 
     this.myPokemon = new Pokemon('charmander', ['fire', 'dragon'], 3, 100, 400, 600, 49, 50, ['tail lash', 'char'], ['all'], true);
+    this.myPokemon2 = new Pokemon('squirtle', ['water'], 3, 100, 400, 600, 49, 50, ['water gun', 'tail whip'], ['all'], true);
 
     this.opponent = new Pokemon('bulbasaur', ['grass', 'dragon'], 3, 100, 400, 600, 49, 50, ['vine whip', 'sunbeam'], ['all'], false);
+
+    this.player = new Player("Ash", [], [this.myPokemon, this.myPokemon2]);
 
     this.commenceBattle();
   }
@@ -34,14 +38,52 @@ export class BattleComponent implements OnInit {
   commenceBattle() {
     let turn = 1;
     do {
-      this.myPokemon.currentHP--);
-      this.checkBattleOver();
+      this.myPokemon.currentHP -= 5;
+      this.myPokemon2.currentHP -= 10;
+      this.checkAllActivePokemonUnconscious();
+      this.checkOpponentKO();
+      console.log("pokemon1 HP: " + this.myPokemon.currentHP);
+      console.log("pokemon2 HP: " + this.myPokemon2.currentHP);
+      // this.checkBattleOver();
     } while (this.battling);
   }
 
-  checkBattleOver() {
-    if(this.myPokemon.currentHP <= 0 || this.opponent.currentHP <= 0) {
+  checkAllActivePokemonUnconscious() {
+    let unconsciousCount = 0;
+    this.player.activePokemon.forEach(function(pokemon) {
+      if(pokemon.currentHP <= 0) {
+        unconsciousCount++;
+      } else {
+        return false;
+      }
+    });
+
+    if (unconsciousCount === this.player.activePokemon.length) {
+      this.battling = false;
+      return true;
+    }
+  }
+
+  checkOpponentKO() {
+    if(this.opponent.currentHP <= 0) {
       this.battling = false;
     }
+  }
+
+  flee() {
+    this.battling = false;
+  }
+
+  catch() {
+    if (this.player.activePokemon.length < 6) {
+      this.player.activePokemon.push(this.opponent);
+    } else {
+      this.player.inactivePokemon.push(this.opponent);
+    }
+    this.battling = false;
+  }
+
+  switchPokemon(selectedPokemon: Pokemon) {
+    this.myPokemon = selectedPokemon;
   }
 }
