7ac4b5d2f3940b5ff1eb9e3b92d00f4c839277b0
Guest User <Guest@epicodus-4.local>
2017-10-03 14:22:51 -0700

    Revising battle component, started work on battle html.

--- src/app/get-pokemon/get-pokemon.component.ts
+++ src/app/get-pokemon/get-pokemon.component.ts
@@ -61,14 +61,15 @@ private user;
     );
   }
 
-  catchTestPokemon(name,types,level,currentLevelXP,totalLevelXP,totalAccruedXP,currentHP,maxHP,speed,attack,defense,activeMoves,allMoves,playerActive){
-    let newPokemon:Pokemon = new Pokemon(name, types,level,currentLevelXP,totalLevelXP,totalAccruedXP,currentHP,maxHP,speed,attack,defense,activeMoves,allMoves,playerActive);
+  catchTestPokemon(name,sprites,types,level,currentLevelXP,totalLevelXP,totalAccruedXP,currentHP,maxHP,speed,attack,defense,activeMoves,allMoves,playerActive){
+    let newPokemon:Pokemon = new Pokemon(name,sprites, types,level,currentLevelXP,totalLevelXP,totalAccruedXP,currentHP,maxHP,speed,attack,defense,activeMoves,allMoves,playerActive);
     console.log(newPokemon);
     this.pokemonService.catchPokemon(newPokemon);
   }
 
   buildPokemon(pokemonToBuild){
     let name = pokemonToBuild.name;
+    let sprites = [''];
     let types = pokemonToBuild.types;
     let level = this.difficulty;
     let currentLevelXP = 0;
@@ -83,7 +84,7 @@ private user;
     let allMoves = pokemonToBuild.moves;
     let playerActive = false;
 
-    let newPokemon:Pokemon = new Pokemon(name, types,level,currentLevelXP,totalLevelXP,totalAccruedXP,currentHP,maxHP,speed,attack,defense,activeMoves,allMoves,playerActive);
+    let newPokemon:Pokemon = new Pokemon(name,sprites, types,level,currentLevelXP,totalLevelXP,totalAccruedXP,currentHP,maxHP,speed,attack,defense,activeMoves,allMoves,playerActive);
 
     console.log(newPokemon);
 
